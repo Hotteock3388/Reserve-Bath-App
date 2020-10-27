bdda4727f95795ae931ed437b08d96ef6c23f519
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2020-10-27 23:02:46 +0900

    [ ADD ] MainActivity Fab에 Intent추가
    MainActivity에 있는 Fab의 Onclick에 NewReserveActivity로 이동하는 메소드 구현

--- app/src/main/java/com/example/reserve_bath_app/MainActivity.kt
+++ app/src/main/java/com/example/reserve_bath_app/MainActivity.kt
@@ -1,13 +1,26 @@
 package com.example.reserve_bath_app
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.github.clans.fab.FloatingActionButton
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        fabSetting()
+    }
 
+    private fun fabSetting() {
+        fab_NewReserve.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this@MainActivity, NewReserveActivity::class.java)
+            intent.putExtra("Data", "")
+            startActivity(intent)
+            overridePendingTransition(R.anim.rightin_activity, R.anim.not_move_activity)
+        })
     }
 }
--- app/src/main/java/com/example/reserve_bath_app/NewReserveActivity.kt
+++ app/src/main/java/com/example/reserve_bath_app/NewReserveActivity.kt
@@ -7,5 +7,7 @@ class NewReserveActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_reserve)
+
+
     }
 }
