96f7c9d66fac52d8b0e46403e094aba6a088f8a4
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2020-10-28 23:25:39 +0900

    [ Add ] DataClass 생성
    예약 정보를 저장할 DataClass 생성

---
+++ app/src/main/java/com/example/reserve_bath_app/DataClass.kt
@@ -0,0 +1,14 @@
+package com.example.reserve_bath_app
+
+data class ReserveData(
+    var date: String,
+    var time: String,
+    var temp: Int
+    )
+
+data class SelectData(
+    var date: String,
+    var day: Int = 0,
+    var time: String,
+    var temp: Int
+)
