cdb2064a21581afdfbee79587e06211a60bc773b
Hotteock <57486593+Hotteock3388@users.noreply.github.com>
2020-11-26 21:42:12 +0900

    Update MainActivity.kt

--- app/src/main/java/com/example/reserve_bath_app/MainActivity.kt
+++ app/src/main/java/com/example/reserve_bath_app/MainActivity.kt
@@ -2,10 +2,7 @@ package com.example.reserve_bath_app
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.AdapterView.OnItemClickListener
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 import java.time.LocalDate
 
@@ -17,8 +14,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        fabInit()
-        RecyclerViewInit()
+        viewPagerInit()
+
         addReserveDatas()
     }
 
@@ -26,45 +23,19 @@ class MainActivity : AppCompatActivity() {
         Singleton.reserveDataList.add(ReserveData(LocalDate.now().plusDays(1),"06", "06", 10))
         Singleton.reserveDataList.add(ReserveData(LocalDate.now().plusDays(6),"12", "11", 20))
         Singleton.reserveDataList.add(ReserveData(LocalDate.now().plusDays(11),"16", "30", 30))
+
+        viewPager.adapter   ?.notifyDataSetChanged()
     }
 
-    private fun RecyclerViewInit() {
-
-//어댑터를 생성한다
-
-//데이터를 생성해야한다(서버통신을 안 할 경우 이렇게)
-        myAdapter = MyAdapter(applicationContext)
-
-//데이터를 어댑터 안에 넣는다
-        myAdapter.reserveDataList = Singleton.reserveDataList
-
-//어댑터의 데이터가 변했다는 notify를 날린다
-        myAdapter.notifyDataSetChanged()
-
-//실제 RecyclerView의 adapter를 만든 adapter로 설정한다
-        recyclerView_PreviousSettings.adapter = myAdapter
-
-
+    private fun viewPagerInit() {
+        viewPager.adapter = ViewPagerAdapter(applicationContext)
+        viewPager.setPageTransformer(false, CustomPageTransformer())
     }
 
-
-    private fun fabInit() {
-        fab_NewReserve.setOnClickListener{
-            val intent = Intent(this, NewReserveActivity::class.java)
-            intent.putExtra("Data", "")
-            startActivityForResult(intent, 100)
-            overridePendingTransition(R.anim.rightin_activity, R.anim.not_move_activity)
-        }
-
-        fab_ShowPreviousSettings.setOnClickListener {
-            startActivityForResult(Intent(this, ViewPagerTestActivity::class.java), 100)
-            overridePendingTransition(R.anim.rightin_activity, R.anim.not_move_activity)
-        }
-    }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        myAdapter.notifyDataSetChanged()
+        //myAdapter.notifyDataSetChanged()
     }
 
 }
